3fb7e3dad59da2d8b9740bd5672ca5c3c17b14f4
Mark Harris <=>
2020-03-06 15:45:28 +0000

    added templates

--- prison_break/prison_break_app/views.py
+++ prison_break/prison_break_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("test prison break app")
+    return render(request, 'index.html')
