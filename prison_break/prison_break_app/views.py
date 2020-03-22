54a2f936db813db6da71b3f3616298cf80a50bf5
Calum <2388714m@student.gla.ac.uk>
2020-03-22 18:02:44 +0000

    Added start of signu up

--- prison_break/prison_break_app/views.py
+++ prison_break/prison_break_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from prison_break_app.models import UserProfile
+from django.contrib.auth.models import User
+
+
 # Create your views here.
 def index(request):
     return render(request, 'prison_break_app/index.html')
@@ -9,3 +13,20 @@ def about(request):
 
 def play(request):
     return render(request, 'prison_break_app/play.html')
+
+def signup(request):
+    return render(request, 'prison_break_app/Signup.html')
+
+def register(request):
+    registered = False
+
+    if request.method == "POST":
+        Username = request.POST.get('inputUserame')
+        email = request.POST.get('inputEmail')
+        Password = request.POST.get('inputPassword')
+        user =  User.objects.create_user(username= Username, password = Password, email= email)
+        user.save()
+
+
+
+    return render(request, 'prison_break_app/signup.html' )
